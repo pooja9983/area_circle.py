bee1917c3c794eed52e9600b8d5ae3431feebbe1
pooja9983 <69471390+pooja9983@users.noreply.github.com>
2021-12-05 12:04:12 +0530

    added files visual studio code

---
+++ python1.py
@@ -0,0 +1,4 @@
+import math as M 
+Radius =float(input("please enter the radius of given circle:"))
+area_of_the_circle=M.pi*Radius*Radius
+print(" the area of the given circle is :", area_of_the_circle)
